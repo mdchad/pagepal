7e1acced5eb9a37406332bc19c8a049ede5faa7a
mdchad <irsyad.muhd@gmail.com>
2023-01-12 01:31:11 +0800

    add search box

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,5 +1,10 @@
 import Link from "next/link";
 import Streak from "./Streak";
+import { PlusIcon } from "@heroicons/react/24/solid";
+import { useState } from "react";
+import {Arrow, useLayer} from "react-laag";
+import {AnimatePresence} from "framer-motion";
+import Search from "./Search";
 
 const books = [
   { title: "Atomic Habits" },
@@ -8,6 +13,25 @@ const books = [
 ];
 
 export default function Main() {
+  const [isOpen, setOpen] = useState(false);
+  const [searchBox, setSearchBox] = useState(false);
+
+  function close() {
+    setOpen(false);
+  }
+
+  const { renderLayer, triggerProps, layerProps, arrowProps } = useLayer({
+    isOpen,
+    onOutsideClick: close, // close the menu when the user clicks outside
+    onDisappear: close, // close the menu when the menu gets scrolled out of sight
+    overflowContainer: false, // keep the menu positioned inside the container
+    auto: true, // automatically find the best placement
+    placement: "top-center", // we prefer to place the menu "top-end"
+    triggerOffset: 12, // keep some distance to the trigger
+    containerOffset: 16, // give the menu some room to breath relative to the container
+    arrowOffset: 16, // let the arrow have some room to breath also
+  });
+
   return (
     <div className="grid sm:grid-cols-3 sm:grid-rows-2 mx-32 mt-24">
       <div className="row-span-2 col-span-2">
@@ -22,25 +46,52 @@ export default function Main() {
           );
         })}
       </div>
-      <aside className="mt-16">
-        <p className="text-lg font-mono mb-6">Reading Streak</p>
-        <div className="grid grid-cols-[repeat(15,minmax(0,1fr))] w-2/3 gap-1">
-          {Array(30).fill(10).map((day, i) => {
-            return (
-              <Streak key={day} day={i} />
-            )
-          })}
+      <aside className="sm:mr-10">
+        <div className="mt-16">
+          <p className="text-lg font-mono mb-6">Reading Streak</p>
+          <div className="grid grid-cols-[repeat(15,minmax(0,1fr))] w-3/4 gap-1">
+            {Array(30)
+              .fill(10)
+              .map((day, i) => {
+                return <Streak key={i} day={i} />;
+              })}
+          </div>
+        </div>
+        <div className="mt-16">
+          <div className="flex justify-between items-center">
+            <p className="text-lg font-mono">Upcoming read</p>
+            <button
+              className="cursor-pointer"
+              {...triggerProps}
+              onMouseEnter={() => setOpen(true)}
+              onMouseLeave={() => setOpen(false)}
+              onClick={() => setSearchBox(true)}
+            >
+              <PlusIcon width={16} height={16} />
+              {renderLayer(
+                <AnimatePresence initial={false}>
+                  {isOpen && (
+                    <div
+                      {...layerProps}
+                      className={`p-2 shadow-md flex flex-col items-center bg-gray-50 rounded-md text-gray-600 whitespace-nowrap`}
+                    >
+                      <p className="font-mono text-xs">Add more books</p>
+                      <Arrow {...arrowProps} size={5} roundness={0} />
+                    </div>
+                  )}
+                </AnimatePresence>
+              )}
+            </button>
+          </div>
+          <div className="mt-6">
+            <p className="text-sm font-mono mb-1">
+              The subtle art of not giving a f*ck
+            </p>
+            <p className="text-sm font-mono">Zero to one</p>
+          </div>
         </div>
       </aside>
-      <div className="mt-16">
-        <p className="text-lg font-mono mb-6">Next books</p>
-        <div>
-          <p className="text-sm font-mono mb-1.5">
-            The subtle art of not giving a f*ck
-          </p>
-          <p className="text-sm font-mono">Zero to one</p>
-        </div>
-      </div>
+      {searchBox && <Search open={searchBox} setOpen={setSearchBox}/>}
     </div>
   );
 }
